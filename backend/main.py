0489528bc51de64ec8febc97e9d08552118f2451
vishwasankar09-del <vishwasankar09@gmail.com>
2026-02-18 17:59:39 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import random
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
@@ -8,6 +17,7 @@ def home():
 
 @app.get("/generate/{rank}")
 def generate_test(rank: int):
+
     if rank <= 1000:
         diff = "Advanced"
     elif rank <= 5000:
@@ -17,8 +27,18 @@ def generate_test(rank: int):
     else:
         diff = "Easy"
 
+    questions = []
+
+    for i in range(10):
+        x = random.randint(2,20)
+        questions.append({
+            "question": f"{diff} JEE Question: If x = {x}, find 3x",
+            "options": [str(3*x), str(4*x), str(5*x), str(6*x)],
+            "answer": str(3*x)
+        })
+
     return {
-        "target_rank": rank,
         "difficulty": diff,
-        "questions": 90
+        "questions": questions
     }
+
